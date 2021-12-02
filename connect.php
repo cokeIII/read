3fd826c74141590ac477ff0d7ddade3dd10b1f2a
cokeIII <coke2484@gmail.com>
2021-12-03 05:29:20 +0700

    f

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "read";
+$database = "chontech2020!";
 // Create connection
 $conn = new mysqli($servername, $username, $password,$database);
 $conn->set_charset("utf8");
