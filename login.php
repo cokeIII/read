9c81cbbdc772a5e65d097d4a5e623ed32a95cc30
cokeIII <coke2484@gmail.com>
2021-12-03 05:53:48 +0700

    f

--- login.php
+++ login.php
@@ -3,16 +3,19 @@ require_once "connect.php";
 session_start();
 $username = $_POST["username"];
 $password = $_POST["password"];
-if($username == "admin" && $password == "ctcread"){
+if ($username == "admin" && $password == "ctcread") {
     $_SESSION["user_status"] = "admin";
     header("location: a_list_log.php");
 } else {
     $sql = "select * from student where student_id = '$username' and birthday='$password' and status = '0'";
-    $res = mysqli_query($conn,$sql);
+    $res = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($res);
-    $_SESSION["student_id"] = $row["student_id"];
-    $_SESSION["user_status"] = "student";
-    header("location: add_log.php");
+    $numRow = mysqli_num_rows($res);
+    if ($numRow > 0) {
+        $_SESSION["student_id"] = $row["student_id"];
+        $_SESSION["user_status"] = "student";
+        header("location: add_log.php");
+    } else {
+        header("location: errPage.php?textErr=ชื่อผู้ใช้ หรือ รหัสผ่านไม่ถูกต้อง กรุณาเข้าสู่ระบบใหม่อีกครั้ง <a href='index.php'>เข้าสู่ระบบ<a/>");
+    }
 }
-
-
